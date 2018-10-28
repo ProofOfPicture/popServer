9931ae49b200df20426bec76a6ac49d57a3538eb
hczerpak <hczerpak@theworkshop.com>
2018-10-28 10:56:52 +0000

    Server with PUT /isValid ok

--- validatePicture.js
+++ validatePicture.js
@@ -3,14 +3,12 @@ let BITBOX = new BITBOXSDK();
 
 class Validator {
     async validatePictureHash(pictureHash, transactionId) {
-        this.isValidPicture = false;
-
         try {
             let details = await BITBOX.Transaction.details(transactionId).catch(error => console.log(error));
             if (details) {
-
-                let pictureHashInBlockchain = details.vout[0].scriptPubKey.hex.toString().replace('6a03', ''); // Ugly I know, but works?
-                return pictureHashInBlockchain === pictureHash;
+                let fromASM = BITBOX.Script.fromASM(details.vout[0].scriptPubKey.asm);
+                let candidate = fromASM.toString("hex").substr(4);
+                return candidate === pictureHash;
             }
         } catch (error) {
             console.error(error)
